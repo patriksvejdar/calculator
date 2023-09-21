de0f535cbfa5cf1f8075496a54b2c1568f6eb85a
patriksvejdar <patrik-svejdar@seznam.cz>
2023-09-21 09:58:25 +0200

    Přidejte soubory projektu.

---
+++ calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿int a, b, c;
+string pocet;
+Console.WriteLine("zadej číslo");
+a = (int) inputValue();
+Console.WriteLine("zadej číslo");
+b = (int)inputValue();
+Console.WriteLine("zadej znak");
+pocet = Console.ReadLine()
+    if (pocet == "+")
+{
+
+}
